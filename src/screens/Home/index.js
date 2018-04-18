24991f0b3af3389896dab00884064327dcc263e0
zainfikrih <useryonoyou@gmail.com>
2018-04-18 14:25:31 +0700

    update first

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,25 +1,55 @@
 //MODULES
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 //ASSETS
-import logo from './logo.svg'
+// import logo from './logo.svg'
+import { Sidebar, Segment, Menu, Icon, Image } from 'semantic-ui-react'
 
 //STYLES
-import styles from './css/index.scss'
+// import styles from './css/index.scss'
 
 //COMPONENT
 class App extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div className={styles.container}>
-        <header className={styles.header}>
+      <Segment>
+        {/* <header className={styles.header}>
           <img src={logo} className={styles.logo} alt="logo" />
           <h1 className={styles.title}>Welcome to React</h1>
         </header>
         <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </p> */}
+        <Sidebar as={Menu} animation='overlay' visible width='thin' icon='labeled' vertical>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
+            <Link to={'#'} >
+              <Image src={'/static/icon/dashboard.svg'} style={{ width: '18px', }} inline verticalAlign='bottom'/>
+                Dashboard
+            </Link>
+          </Menu.Item>
+          <Menu.Item name='menu' active={activeItem === 'menu'} onClick={this.handleItemClick}>
+            <Link to={'#'} >
+              <Image src={'/static/icon/dashboard.svg'} style={{ width: '18px', }} inline verticalAlign='bottom'/>
+              Menu
+            </Link>
+          </Menu.Item>
+          <Menu.Item name='report' active={activeItem === 'report'} onClick={this.handleItemClick}>
+            <Link to={'#'} >
+              <Image src={'/static/icon/dashboard.svg'} style={{ width: '18px', }} inline verticalAlign='bottom'/>  
+              Report
+            </Link>
+          </Menu.Item>
+        </Sidebar>
+
+      </Segment>
     )
   }
 }
