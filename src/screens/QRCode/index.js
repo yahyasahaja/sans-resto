7ec5c0dda991317d154dd25535e871cc0318fcf2
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-04-20 22:52:44 +0700

    update menu router and fix warning

--- src/screens/QRCode/index.js
+++ src/screens/QRCode/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import QRCode from 'qrcode.react'
 
-import Table from '../../components/Table'
+// import Table from '../../components/Table'
 
 export default class componentName extends Component {
   render() {
     return (
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
-        
         <h1 style={{textAlign: 'center', marginBottom: 50, marginTop: 50}}>CAFETARIA UB</h1>
         <QRCode value="scanresto" style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
       </div>
