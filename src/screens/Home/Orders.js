dd5861ffd08a5cf28d48abfd20958f9a90ae7c13
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-04-20 23:58:16 +0700

    update table and card

--- src/screens/Home/Orders.js
+++ src/screens/Home/Orders.js
@@ -45,17 +45,55 @@ const tableData = [
     'Vivy Junita',
     '10',
   ],
+  [
+    'SC003',
+    'Vivy Junita',
+    '10',
+  ],
+  [
+    'SC003',
+    'Vivy Junita',
+    '10',
+  ],
 ]
+
+//INNER_CONFIG
+const MAX_ITEMS = 5
 
 //COMPONENT
 export default class Orders extends Component {
+  state = {
+    tableData: tableData.slice(0, 5),
+    loading: false
+  }
+
+  handleChange = activePage => {
+    this.setState({loading: true})
+    setTimeout(() => {
+      this.setState({
+        tableData: tableData.slice(
+          (activePage - 1) * MAX_ITEMS, (activePage - 1) * MAX_ITEMS + MAX_ITEMS
+        ),
+        loading: false
+      })
+    }, 1000)
+  }
+
   render() {
     return (
       <Card className={styles.card} >
         <div className={styles.header} >
           <h1>Orders</h1>
         </div>
-        <Table headers={tableHeaders} data={tableData} />
+        <Table 
+          headers={tableHeaders} 
+          data={this.state.tableData} 
+          pagination
+          totalPages={Math.ceil(tableData.length / MAX_ITEMS)}
+          onPageChange={this.handleChange}
+          defaultWidth={500}
+          loading={this.state.loading}
+        />
       </Card>
     )
   }
