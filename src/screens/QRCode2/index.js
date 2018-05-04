91eecb5376a93689b1e3d8d5557f1a7b36b89752
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-05-05 02:35:09 +0700

    update table

--- src/screens/QRCode2/index.js
+++ src/screens/QRCode2/index.js
@@ -6,7 +6,7 @@ export default class componentName extends Component {
     return (
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
         <h1 style={{textAlign: 'center', marginBottom: 50, marginTop: 50}}>TABLE SCAN</h1>
-        <QRCode value="scantable" style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
+        <QRCode value="1" style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
       </div>
     )
   }
