0cdd47f12f88072598b94a1e7a090a60c12ac6e6
zainfikrih <useryonoyou@gmail.com>
2018-05-04 21:02:54 +0700

    menus

--- src/screens/Menu/Menus.js
+++ src/screens/Menu/Menus.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Search, Image } from 'semantic-ui-react'
 import _ from 'lodash'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 //STYLES
 import styles from './css/menus.scss'
@@ -11,125 +12,19 @@ import styles from './css/menus.scss'
 import Card from '../../components/Card'
 import Table from '../../components/Table'
 
+//CONFIG
+import { GRAPHQL_END_POINT } from '../../config'
+import index from '../../services/stores';
+
+// //STORE 
+// import { token } from '../../services/stores'
+
 const tableHeaders = [
-  'Menu',
+  'Menu', 
   'Name',
   'Price (Rp.)',
 ]
 
-const tableData = [
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-  [
-    <div className={styles.img} >
-      <Image className={styles.imgMenus} src="/static/image/sate.png" />
-    </div>,
-    <div className={styles.title}>Sate Ayam<div className={styles.desc}>10 tusuk</div></div>,
-    <div className={styles.delete} >15.000 
-      <span className={`material-icons ${styles.icon1}`}>settings</span>
-      <span className={`material-icons ${styles.icon}`}>delete</span>
-    </div>,
-  ],
-
-]
 
 //INNER_CONFIG
 const MAX_ITEMS = 8
@@ -137,20 +32,60 @@ const MAX_ITEMS = 8
 //COMPONENT
 export default class Orders extends Component {
   state = {
-    tableData: tableData.slice(0, MAX_ITEMS),
-    loading: false
+    //tableData: tableData.slice(0, MAX_ITEMS),
+    loading: false,
+    // name: {},
+    // description: {},
+    // price: {},
+    // image: {},
+    menus: [],
+    restaurant_id: 1,
+  }
+
+  componentDidMount() {
+    axios.post(GRAPHQL_END_POINT, {
+      query: `
+      query {
+        allRestaurantMenus (restaurant_id:"${this.state.restaurant_id}") {
+          name
+          description
+          price
+          image
+        }
+      }
+      `
+    }).then(({data}) => {
+      if(!data) return
+
+      return console.log(data)
+      this.setState({
+        menus: data.data.allRestaurantMenus.map((menu)=>{
+          return[
+            <div className={styles.img} >
+              <Image className={styles.imgMenus} src="/static/image/sate.png" />
+            </div>,
+            <div className={styles.title}>{menu.name}<div className={styles.desc}>{menu.description}</div></div>,
+            <div className={styles.delete} >{menu.price} 
+              <span className={`material-icons ${styles.icon1}`}>settings</span>
+              <span className={`material-icons ${styles.icon}`}>delete</span>
+            </div>,
+          ]
+        })
+      })
+
+    })
   }
 
   handleChange = activePage => {
-    this.setState({loading: true})
-    setTimeout(() => {
-      this.setState({
-        tableData: tableData.slice(
-          (activePage - 1) * MAX_ITEMS, (activePage - 1) * MAX_ITEMS + MAX_ITEMS
-        ),
-        loading: false
-      })
-    }, 1000)
+    // this.setState({loading: true})
+    // setTimeout(() => {
+    //   this.setState({
+    //     tableData: tableData.slice(
+    //       (activePage - 1) * MAX_ITEMS, (activePage - 1) * MAX_ITEMS + MAX_ITEMS
+    //     ),
+    //     loading: false
+    //   })
+    // }, 1000)
   }
 
   handleSearchChange = search => {
@@ -191,9 +126,9 @@ export default class Orders extends Component {
         <div className={styles.right} >
           <Table 
             headers={tableHeaders} 
-            data={this.state.tableData} 
+            data={this.state.menus} 
             pagination
-            totalPages={Math.ceil(tableData.length / MAX_ITEMS)}
+            totalPages={Math.ceil(this.state.menus.length / MAX_ITEMS)}
             onPageChange={this.handleChange}
             defaultWidth={500}
             loading={this.state.loading}
