574d995cf8939950c17166a38b9bfe53db94f0f5
zainfikrih <useryonoyou@gmail.com>
2018-05-05 08:05:12 +0700

    done2

--- src/screens/QRCode/index.js
+++ src/screens/QRCode/index.js
@@ -53,7 +53,7 @@ export default class componentName extends Component {
     return (
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
         <h1 style={{textAlign: 'center', marginBottom: 50, marginTop: 50}}>Aneka Sate</h1>
-        <QRCode value="scanresto" style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
+        <QRCode value={this.state.uuid} style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
       </div>
     )
   }
