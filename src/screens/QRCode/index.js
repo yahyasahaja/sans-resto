85325317aeb551d12267eafc6e8161d467aa53a2
zainfikrih <useryonoyou@gmail.com>
2018-05-05 07:47:10 +0700

    update

--- src/screens/QRCode/index.js
+++ src/screens/QRCode/index.js
@@ -7,7 +7,7 @@ export default class componentName extends Component {
   render() {
     return (
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}>
-        <h1 style={{textAlign: 'center', marginBottom: 50, marginTop: 50}}>CAFETARIA UB</h1>
+        <h1 style={{textAlign: 'center', marginBottom: 50, marginTop: 50}}>Aneka Sate</h1>
         <QRCode value="scanresto" style={{width: 300, height: 300, margin: 'auto', marginTop: 10}} />
       </div>
     )
